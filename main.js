f09bc4f46cefe5829b2f5178f100156ac10d1ee8
Marian <95725615+Mariangdev@users.noreply.github.com>
2022-02-16 21:53:27 -0300

    agregando

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-class Jugador {
-    constructor(nombre, camiseta, edad, lesionado) {
-        this.nombre = nombre;
-        this.camiseta = camiseta;
-        this.edad = edad;
-        this.lesionado = lesionado;
-    }
+function impuesto(precio, porcentaje){
+    return precio + ((precio * porcentaje)/100)
 }
-const jugadores = [];
-jugadores.push(new Jugador("Cris", 15 , 31, false));
-jugadores.push(new Jugador("Facu", 2 , 22, false));
-jugadores.push(new Jugador("Mari", 68 , 22, true));
-jugadores.push(new Jugador("Charly", 0 , 23, false));
-jugadores.push(new Jugador("Ailu", 98 , 24, false));
-jugadores.push(new Jugador("Cin", 98 , 48, false));
-console.log(jugadores);
+for (let index = 0; index < 5; index++) {
+    let resultado = impuesto(parseFloat(prompt("Ingresar precio"))
+  ,parseFloat(prompt("Ingresar %")));
+    alert(resultado);
+}
